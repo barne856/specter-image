be8335310fbf48bf9c4eca87115ff1652eb477ca
Brendan Barnes <brendan.barnes856@gmail.com>
2024-11-04 00:27:38 -0600

    save

--- build.sh
+++ build.sh
@@ -4,28 +4,11 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen 
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
-
-# Install Cosmic Desktop
+# Install Packages
 curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$RELEASE/ryanabx-cosmic-epoch-fedora-$RELEASE.repo
+curl -Lo /etc/yum.repos.d/_copr_proletarius101-firefox-nightly.repo https://copr.fedorainfracloud.org/coprs/proletarius101/firefox-nightly/repo/fedora-$RELEASE/proletarius101-firefox-nightly-fedora-$RELEASE.repo
 rpm-ostree install cosmic-desktop
-rpm-ostree install gnome-keyring NetworkManager-tui NetworkManager-openvpn
+rpm-ostree install gnome-keyring NetworkManager-tui NetworkManager-openvpn firefox-nightly
 systemctl disable gdm || true
 systemctl disable sddm || true
 systemctl enable cosmic-greeter
