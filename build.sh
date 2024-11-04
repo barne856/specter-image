72f5c30ee9e478f102765b778b072f35dfdefcc5
Brendan Barnes <brendan.barnes856@gmail.com>
2024-11-04 17:43:54 -0600

    save

--- build.sh
+++ build.sh
@@ -5,13 +5,6 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install Packages
-curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$RELEASE/ryanabx-cosmic-epoch-fedora-$RELEASE.repo
 curl -Lo /etc/yum.repos.d/_copr_proletarius101-firefox-nightly.repo https://copr.fedorainfracloud.org/coprs/proletarius101/firefox-nightly/repo/fedora-$RELEASE/proletarius101-firefox-nightly-fedora-$RELEASE.repo
-rpm-ostree install cosmic-desktop
-rpm-ostree install gnome-keyring NetworkManager-tui NetworkManager-openvpn firefox-nightly
-systemctl disable gdm || true
-systemctl disable sddm || true
-systemctl enable cosmic-greeter
+rpm-ostree install firefox-nightly
 ostree container commit
-mkdir -p /var/tmp
-chmod -R 1777 /var/tmp
