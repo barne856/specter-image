e759dd5f8c26c91d113e811d29c7757e4bda2597
Brendan Barnes <brendan.barnes856@gmail.com>
2024-11-10 19:44:02 -0600

    save

--- build.sh
+++ build.sh
@@ -5,9 +5,4 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install Packages
-# Brave Browser
-curl -Lo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-curl -Lo /etc/pki/rpm-gpg/brave-core.asc https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-rpm-ostree install brave-browser
-ostree container commit
 # env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia  --enable-unsafe-webgpu --enable-features=Vulkan
